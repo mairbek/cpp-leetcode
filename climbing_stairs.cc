296e00c6ff71d04a52606a44d2e9d652914dd6ef
Mairbek Khadikov <mkhadikov@gmail.com>
2020-05-04 19:36:45 -0700

    fibs

---
+++ climbing_stairs.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+
+namespace {
+// https://leetcode.com/problems/climbing-stairs/
+// easy fibonacci
+class Solution {
+public:
+  int fib(int n) {
+    int a = 1, b = 1;
+    for (int i = 1; i < n; i++) {
+      int nb = a + b;
+      a = b;
+      b = nb;
+    }
+    return b;
+  }
+ int climbStairs(int n) {
+   return fib(n);
+ }
+};
+} // namespace
+
+using int64 = long long;
+
+void solve(int n) {
+  Solution sol;
+  int result = sol.climbStairs(n);
+  std::cout << result << std::endl;
+}
+
+int main(int argc, const char** argv) {
+  solve(2);
+  solve(3);
+  solve(4);
+  solve(5);
+  return 0;
+}
---
+++ first_bad_version.cc
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+
+// https://leetcode.com/problems/first-bad-version/
+// simple binary search.
+namespace {
+class Solution {
+public:
+  bool isBadVersion(int n) {
+    return n >= 3;
+  }
+
+  int firstBadVersion(int n) {
+    int l = 1, r = n;
+    while (l <= r) {
+      int m = (l / 2) + (r / 2) + ((l % 2 + r % 2) / 2);
+      if (isBadVersion(m)) {
+        r = m - 1;
+      } else {
+        l = m + 1;
+      }
+    }
+    return l;
+  }
+};
+} // namespace
+
+void solve(int n) {
+  Solution sol;
+  int result = sol.firstBadVersion(n);
+
+  std::cout << n << " " << result << std::endl;
+}
+int main(int argc, const char** argv) {
+  solve(5);
+  return 0;
+}
