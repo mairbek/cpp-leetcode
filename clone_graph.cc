32a46afa24a8fe0f83df8ec39dc0afebed90c926
Mairbek Khadikov <mkhadikov@gmail.com>
2020-05-08 21:17:18 -0700

    clone graph

---
+++ clone_graph.cc
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+// https://leetcode.com/problems/clone-graph/
+// clone graph, recursive.
+namespace {
+class Node {
+ public:
+  int val;
+  std::vector<Node*> neighbors;
+  Node() {
+    val = 0;
+    neighbors = std::vector<Node*>();
+  }
+
+  Node(int _val) {
+    val = _val;
+    neighbors = std::vector<Node*>();
+  }
+
+  Node(int _val, std::vector<Node*> _neighbors) {
+    val = _val;
+    neighbors = _neighbors;
+  }
+};
+
+class Solution {
+ public:
+  Node* cloneGraph(Node* node) {
+    if (node == nullptr) {
+      return nullptr;
+    }
+    std::unordered_map<Node*, Node*> visited;
+
+    return doCloneGraph(node, visited);
+  }
+
+  Node* doCloneGraph(Node* node, std::unordered_map<Node*, Node*>& visited) {
+    auto it = visited.find(node);
+
+    if (it != visited.end()) {
+      return it->second;
+    }
+    Node* clone = new Node(node->val);
+    visited[node] = clone;
+
+    for (int i = 0; i < node->neighbors.size(); i++) {
+      Node* n = node->neighbors[i];
+      Node* nclone = doCloneGraph(n, visited);
+      clone->neighbors.push_back(nclone);
+    }
+    return clone;
+  }
+};
+} // namespace
+
+int main(int argc, const char** argv) {
+  return 0;
+}
