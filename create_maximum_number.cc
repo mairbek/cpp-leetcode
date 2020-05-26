2ad9b0f05ae2ee98c8bdcb1f9afe20c3655c4bb7
Mairbek Khadikov <mkhadikov@gmail.com>
2020-05-25 19:42:52 -0700

    DP that works

--- create_maximum_number.cc
+++ create_maximum_number.cc
@@ -3,102 +3,116 @@
 #include <iostream>
 #include <vector>
 
-// https://leetcode.com/problems/triangle/
-// DP O(n*m) time O(n) space.
+// https://leetcode.com/problems/create-maximum-number/
+// DP O(n*k) preproc, O(k**2) solution, time O(nk) space.
+// fking struggle.
 namespace {
-struct Node {
-  int val;
-  Node* prev;
 
-  Node(int val, Node* prev) : val(val), prev(prev) {}
-  Node(int val) : val(val), prev(nullptr) {}
-
-  int cmp(const Node& other) const {
-    int result = 0;
-    const Node* a = this;
-    const Node* b = &other;
-    while (a != nullptr && b != nullptr) {
-      if (a->val == b->val) {
-      } else {
-        result = a->val < b->val ? -1 : 1;
-      }
-      a = a->prev;
-      b = b->prev;
-    }
-    return result;
-  }
-};
+using Matrix = std::vector<std::vector<int>>;
 
 class Solution {
  public:
-  // fuck me,
-  // use segment fucking tree????
-  // use other DP relation?
+  Matrix build_vecs(std::vector<int>& a, int k) {
+    Matrix result;
+    int an = a.size();
+    int ak = std::min(k, an);
 
-  Node* find_max(std::initializer_list<Node*> nodes) {
-    auto it = nodes.begin();
-    Node* result = *it;
-    for (; it != nodes.end(); ++it) {
-      if (result->cmp(**it) < 0) {
-        result = *it;
+    result.push_back(std::vector<int>(an, 0));
+    for (int ik = 0; ik < ak; ik++) {
+      result.push_back(std::vector<int>(an - ik));
+      int prev = -1;
+      int idx = -1;
+      for (int i = an - ik - 1; i >= 0; i--) {
+        if (idx == -1 || a[i] >= prev) {
+          idx = i;
+          prev = a[i];
+        }
+        result[ik][i] = idx;
       }
     }
     return result;
   }
 
-  std::string to_string(Node* node) {
-    std::string result;
-    while (node != nullptr && node->val >= 0) {
-      result += std::to_string(node->val);
-      node = node->prev;
+  std::vector<int> vecof(Matrix& m, std::vector<int>& a, int k) {
+    std::vector<int> result;
+    int i = 0;
+    for (int ik = k - 1; ik >= 0; ik--) {
+      int idx = m[ik][i];
+      result.push_back(a[idx]);
+      i = idx + 1;
     }
-    std::reverse(result.begin(), result.end());
     return result;
+  }
+
+  std::vector<int> merge(std::vector<int>& a, std::vector<int>& b) {
+    std::vector<int> result;
+    int ai = 0;
+    int bi = 0;
+    while (ai < a.size() || bi < b.size()) {
+      if (ai >= a.size()) {
+        result.push_back(b[bi++]);
+        continue;
+      }
+      if (bi >= b.size()) {
+        result.push_back(a[ai++]);
+        continue;
+      }
+      if (lookahead_gt(a, b, ai, bi)) {
+        result.push_back(a[ai++]);
+      } else {
+        result.push_back(b[bi++]);
+      }
+    }
+    return result;
+  }
+
+  bool lookahead_gt(std::vector<int>& a, std::vector<int>& b, int ai, int bi) {
+    while (ai < a.size() && bi < b.size()) {
+      if (a[ai] != b[bi]) {
+        break;
+      }
+      ai++;
+      bi++;
+    }
+    if (ai < a.size() && bi < b.size()) {
+      return a[ai] > b[bi];
+    }
+    return bi >= b.size();
+  }
+
+  bool gt(std::vector<int>& a, std::vector<int>& b) {
+    for (int i = 0; i < a.size(); i++) {
+      if (a[i] > b[i]) {
+        return true;
+      }
+      if (a[i] < b[i]) {
+        return false;
+      }
+    }
+    return false;
   }
 
   std::vector<int> maxNumber(std::vector<int>& a, std::vector<int>& b, int k) {
+    Matrix am = build_vecs(a, k);
+    Matrix mb = build_vecs(b, k);
     int an = a.size();
     int bn = b.size();
 
-    Node* root = new Node(-1);
+    std::vector<int> result;
+    for (int ak = 0; ak <= std::min(k, an); ak++) {
+      int bk = k - ak;
 
-    Node* ndp[k + 1][an + 1][bn + 1];
-    for (int i = 0; i <= an; i++) {
-      for (int j = 0; j <= bn; j++) {
-        ndp[0][i][j] = root;
-      }
-    }
-
-    for (int ik = 1; ik <= k; ik++) {
-      ndp[ik][0][0] = root;
-      for (int i = 0; i < an; i++) {
-        ndp[ik][i + 1][0] =
-            find_max({ndp[ik][i][0], new Node(a[i], ndp[ik - 1][i][0])});
-      }
-      for (int j = 0; j < bn; j++) {
-        ndp[ik][0][j + 1] =
-            find_max({ndp[ik][0][j], new Node(b[j], ndp[ik - 1][0][j])});
+      if (bk > bn) {
+        continue;
       }
 
-      for (int i = 0; i < an; i++) {
-        for (int j = 0; j < bn; j++) {
-          // int64 previ = i >= ik ? dp[ik][i][j + 1] : 0;
-          // int64 prevj = j >= ik ? dp[ik][i + 1][j] : 0;
-          ndp[ik][i + 1][j + 1] =
-              find_max({// previ, prevj,
-                        ndp[ik][i][j + 1], ndp[ik][i + 1][j],
-                        new Node(a[i], ndp[ik - 1][i][j]),
-                        new Node(a[i], ndp[ik - 1][i][j + 1]),
-                        new Node(b[j], ndp[ik - 1][i][j]),
-                        new Node(b[j], ndp[ik - 1][i + 1][j])});
-        }
+      std::vector<int> av = vecof(am, a, ak);
+      std::vector<int> bv = vecof(mb, b, bk);
+      std::vector<int> rr = merge(av, bv);
+
+      if (result.empty() || gt(rr, result)) {
+        result = rr;
       }
-    }
-    Node* node = ndp[k][an][bn];
-    std::vector<int> result(k, 0);
-    for (int i = 0; i < k; i++) {
-      result[k - i - 1] = node->val;
-      node = node->prev;
     }
 
     return result;
@@ -136,19 +150,19 @@ void solve(std::vector<int> a, std::vector<int> b, int k) {
 }
 
 int main(int argc, const char** argv) {
-  // Node a(3, new Node(4));
-  // Node b(2, new Node(4));
-  // std::cout << b.cmp(a) << std::endl;
-  // solve(std::vector<int>{8, 6}, std::vector<int>{9, 1, 7}, 5);
+  solve(std::vector<int>{8, 6}, std::vector<int>{9, 1, 7}, 5);
   solve(std::vector<int>{1, 3}, std::vector<int>{2, 4}, 2);
-  // solve(std::vector<int>{3, 4, 6, 5}, std::vector<int>{9, 1, 2, 5, 8, 3}, 2);
-  // solve(std::vector<int>{3, 4, 6, 5}, std::vector<int>{9, 1, 2, 5, 8, 3}, 3);
-  // solve(std::vector<int>{3, 4, 6, 5}, std::vector<int>{9, 1, 2, 5, 8, 3}, 5);
+  solve(std::vector<int>{3, 4, 6, 5}, std::vector<int>{9, 1, 2, 5, 8, 3}, 2);
+  solve(std::vector<int>{3, 4, 6, 5}, std::vector<int>{9, 1, 2, 5, 8, 3}, 3);
+  solve(std::vector<int>{3, 4, 6, 5}, std::vector<int>{9, 1, 2, 5, 8, 3}, 5);
 
-  // solve(std::vector<int>{6, 7}, std::vector<int>{6, 0, 4}, 5);
-  // solve(std::vector<int>{3, 9}, std::vector<int>{8, 9}, 3);
-  // solve(std::vector<int>{8,5,9,5,1,6,9},
-  // std::vector<int>{2,6,4,3,8,4,1,0,7,2,9,2,8}, 20);
+  solve(std::vector<int>{6, 7}, std::vector<int>{6, 0, 4}, 5);
+  solve(std::vector<int>{6, 6, 7}, std::vector<int>{6, 0, 4}, 6);
+  solve(std::vector<int>{6, 7}, std::vector<int>{6, 6, 0, 4}, 6);
+  solve(std::vector<int>{3, 9}, std::vector<int>{8, 9}, 3);
+  solve(std::vector<int>{8, 6, 9}, std::vector<int>{1, 7, 5}, 3);
+  solve(std::vector<int>{8, 5, 9, 5, 1, 6, 9},
+        std::vector<int>{2, 6, 4, 3, 8, 4, 1, 0, 7, 2, 9, 2, 8}, 20);
   solve(std::vector<int>{1, 5, 8, 1, 4, 0, 8, 5, 0, 7, 0, 5, 7, 6, 0, 5,
                          5, 2, 4, 3, 6, 4, 6, 6, 3, 8, 1, 1, 3, 1, 3, 5,
                          4, 3, 9, 5, 0, 3, 8, 1, 4, 9, 8, 8, 3, 4, 6, 2,
@@ -160,5 +174,10 @@ int main(int argc, const char** argv) {
                          9, 7, 1, 4, 4, 1, 4, 0, 2, 3, 1, 3, 2, 0, 2, 4,
                          0, 9, 2, 0, 1, 3, 9, 1, 2, 2, 6, 6, 9, 3, 6, 0},
         80);
+
+  solve(std::vector<int>{6, 3, 9, 0, 5, 6},
+        std::vector<int>{2, 2, 5, 2, 1, 4, 4, 5, 7, 8, 9, 3, 1, 6, 9, 7, 0},
+        23);
+
   return 0;
 }
