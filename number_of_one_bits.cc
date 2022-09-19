47830ce01c5ded25bf985466745759e61e022922
Mairbek Khadikov <mkhadikov@gmail.com>
2022-09-19 15:31:22 -0700

    number of 1 bits using Kernighan algo

---
+++ number_of_one_bits.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+
+namespace {
+class Solution {
+ public:
+    int hammingWeight(uint32_t n) {
+        int result = 0;
+        while (n > 0) {
+            n = n & (n-1);
+            result++;
+        }
+        return result;
+    }
+};
+}  // namespace
+
+void solve(uint32_t n) {
+  Solution sol;
+  int result = sol.hammingWeight(n);
+
+  std::cout << n << " " << result << std::endl;
+}
+int main(int argc, const char** argv) {
+  solve(0b00000000000000000000000000000000);
+  solve(0b00000000000000000000000000001011);
+  solve(0b00000000000000000000000010000000);
+  solve(0b11111111111111111111111111111101);
+  return 0;
+}
