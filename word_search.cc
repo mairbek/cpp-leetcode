6f8b0673609f63e005b86cfbfde03e567f63bc84
Mairbek Khadikov <mairbek@brex.com>
2022-09-22 17:46:34 -0700

    small opt

--- word_search.cc
+++ word_search.cc
@@ -41,10 +41,10 @@ class Solution {
   bool exist(std::vector<std::vector<char>>& board, std::string word) {
     int n = board.size();
     int m = board[0].size();
+    std::vector<std::vector<bool>> visited(n, std::vector<bool>(m, false));
     for (int i = 0; i < n; i++) {
       for (int j = 0; j < m; j++) {
         if (board[i][j] == word[0]) {
-          std::vector<std::vector<bool>> visited(n, std::vector<bool>(m, false));
           if (dfs(board, word, visited, 0, i, j)) {
             return true;
           }
