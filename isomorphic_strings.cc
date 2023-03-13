d1a67f6d7a763cae6c7a53891f6720172d5794dd
Mairbek Khadikov <mkhadikov@gmail.com>
2023-03-12 20:33:39 -0700

    isomorphic strings

---
+++ isomorphic_strings.cc
@@ -0,0 +1,47 @@
+#include <algorithm>
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+
+namespace {
+class Solution {
+ public:
+  bool isIsomorphic(std::string s, std::string t) {
+    if (s.size() != t.size()) {
+      return false;
+    }
+    std::unordered_map<char, char> map;
+    std::unordered_map<char, char> reverse;
+    for (int i = 0; i < s.size(); i++) {
+      auto it = map.find(s[i]);
+      auto rit = reverse.find(t[i]);
+      if (it == map.end() && rit == reverse.end()) {
+        map[s[i]] = t[i];
+        reverse[t[i]] = s[i];
+      } else {
+        if (it == map.end() || rit == reverse.end()) {
+          return false;
+        }
+        if (it->second != t[i] || rit->second != s[i]) {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+};
+}  // namespace
+
+void solve(std::string s, std::string t) {
+  Solution sol;
+  auto result = sol.isIsomorphic(s, t);
+  std::cout << s << " " << t << " " << result << std::endl;
+}
+
+int main(int argc, char const** argv) {
+  solve("egg", "add");
+  solve("foo", "bar");
+  solve("paper", "title");
+  solve("badc", "baba");
+  return 0;
+}
