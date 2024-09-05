a49307d81e62ca81e446daaf1af322eec0716286
Mairbek Khadikov <mkhadikov@gmail.com>
2024-09-04 22:56:14 -0700

    random_pick_with_weight works

--- random_pick_with_weight.cc
+++ random_pick_with_weight.cc
@@ -14,25 +14,18 @@ class Solution {
     }
   }
 
-  int pickIndex() { 
+  int pickIndex() {
     int wi = rand() % total_;
     int l = 0;
     int r = prefix_sum_.size();
     while (l < r) {
-      int m = (l + r) / 2;
-      // std::cout << "m=" << m << ", prefix=" << prefix_sum_[m] << " wtf " << (wi < prefix_sum_[m]) << std::endl;
-      if (prefix_sum_[m] == wi) {
-        return m;
-      }
+      int m = l + (r - l) / 2;
       if (wi < prefix_sum_[m]) {
-        r = m - 1;
-        // std::cout << "new_r=" << r << std::endl; 
+        r = m;
       } else {
         l = m + 1;
-        // std::cout << "new_l=" << l << std::endl; 
       }
     }
-    std::cout << "wi=" << wi << " i=" << l << std::endl;
     return l;
   }
 
@@ -54,6 +47,6 @@ void solve(std::vector<int> nums, int k) {
 int main(int argc, char const** argv) {
   // solve(std::vector<int>{1}, 1);
   // solve(std::vector<int>{1, 3}, 5);
-  solve(std::vector<int>{3,14,1,7}, 10);
+  solve(std::vector<int>{3, 14, 1, 7}, 10);
   return 0;
 }
